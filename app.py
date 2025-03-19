d137d24193ab8685136e8cdd76d795d00e22d8dd
Daniel Soltys <danielsytlos@gmail.com>
2025-03-19 22:01:01 +0100

    Added main functionality

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
-from flask import Flask, render_template, request
-from trading_strategy_tester.strategy.strategy import Strategy
+from flask import Flask, render_template, request, jsonify
+from trading_strategy_tester.llm_communication.prompt_processor import process_prompt
+from trading_strategy_tester.enums.llm_model_enum import LLMModel
+from trading_strategy_tester.trade.trade import Trade
+import plotly
+import json
+
 
 app = Flask(__name__)
 
-
-@app.route('/', methods=["GET", "POST"])
+@app.route('/')
 def index():
-    result = None
-    if request.method == "POST":
-        # Get the user's input from the form
-        user_input = request.form.get("prompt")
+    return render_template("index.html")
 
-        # Process the input using your library
-        strategy_instance = Strategy()
-        # Assuming your Strategy class has a method like process_prompt
-        result = strategy_instance.process_prompt(user_input)
+@app.route('/process_prompt', methods=["POST"])
+def get_results():
+    user_input = request.json.get("prompt")
+    trades, graphs, stats = process_prompt(user_input, LLMModel.LLAMA_1B)
 
-    return render_template("index.html", result=result)
+    graph_json = []
+    for _, plots in graphs.items():
+        if isinstance(plots, list):
+            for plot_obj in plots:
+                fig = plot_obj.get_plot()
+                graph_json.append(json.loads(plotly.io.to_json(fig)))
+        else:
+            fig = plots.get_plot()
+            graph_json.append(json.loads(plotly.io.to_json(fig)))
 
+    trades_str = [str(trade) for trade in trades]
 
+    return jsonify({"trades": trades_str, "graphs": graph_json, "stats": stats})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
